60c701b1651973a40a2a38787762584dcea740fa
Oscar Cambieri <oscar@cambieri.it>
2013-05-28 00:32:33 +0200

    perfezionamenti; preparato clienti, staff, certificazioni su azienda

--- fondstamp/apps/main/static/scripts/preview.js
+++ fondstamp/apps/main/static/scripts/preview.js
@@ -1,46 +1,42 @@
 /*
  * Image preview script 
- * powered by jQuery (http://www.jquery.com)
- * 
- * written by Alen Grakalic (http://cssglobe.com)
- * 
- * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
- *
  */
- 
-this.imagePreview = function(){	
-	/* CONFIG */
-		
-		xOffset = 10;
-		yOffset = 30;
-		
-		// these 2 variable determine popup's distance from the cursor
-		// you might want to adjust to get the right result
-		
-	/* END CONFIG */
-	$("a.preview").hover(function(e){
-		this.t = this.title;
-		this.title = "";	
-		var c = (this.t != "") ? "<br/>" + this.t : "";
-		$(".container").append("<p id='preview'><img src='"+ this.href +"' alt='Preview' />"+ c +"</p>");								 
+
+this.imagePreview = function() {
+	$("a.preview").hover(function(e) {
+		var img = new Image();
+		img.id = "preview";
+		img.onload = function() {
+			var pos_x = ($(window).width() - $("#preview").outerWidth(true)) / 2;
+			if (pos_x < 0) pos_x = 0;
+			var pos_y = this.src.indexOf("schema") == -1 ? ($(window).height() - $("#preview").outerHeight()) / 2 : parseInt($("body").css("padding-top").replace("px", ""));
+			if (pos_y < 0) pos_y = 0;
+			$("#preview")
+				.css("top", pos_y + "px")
+				.css("left", pos_x + "px")
+				.fadeIn("fast");
+		}
+		$("body").append(img);
+		var margin = parseInt($("#preview").css("margin-top").replace("px", ""));
+		var padding = parseInt($("#preview").css("padding-left").replace("px", ""));
+		var border = parseInt($("#preview").css("border-left-width").replace("px", ""));
+		var delta = margin*2 + padding*2 + border*2;
 		$("#preview")
-// 			.css("top",(e.pageY - xOffset) + "px")
-// 			.css("left",(e.pageX + yOffset) + "px")
-			.fadeIn("fast");						
+			.css("max-width", $(window).width() - delta + "px")
+			.css("max-height", $(window).height() - delta + "px");
+		img.src = this.href;
     },
-	function(){
-		this.title = this.t;	
+	function() {
 		$("#preview").remove();
-    });	
+    });
 //	$("a.preview").mousemove(function(e){
 //		$("#preview")
 // 			.css("top",(e.pageY - xOffset) + "px")
 // 			.css("left",(e.pageX + yOffset) + "px");
-//	});			
+//	});
 };
 
-
 // starting the script on page load
-$(document).ready(function(){
+$(document).ready(function() {
 	imagePreview();
 });
